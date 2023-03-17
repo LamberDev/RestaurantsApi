d46a772d75ccd2fca37525f57e2ef205dcace4cd
Lamberto Lemos <lamberto.lemos@savia.net>
2023-03-17 09:35:14 +0100

    Change class to record on DTO

--- Application/DTOs/RestaurantDTO.cs
+++ Application/DTOs/RestaurantDTO.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Application.DTOs
 {
-    public class RestaurantDTO
+    public record RestaurantDTO
     {   
         public int Id { get; set; }
         public string Name { get; set; }
